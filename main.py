ca5f1dbc3672b6c644f55fc3ba412c054b548466
daniel.gualtero <daniel.gualtero@gmail.com>
2020-07-02 23:37:12 -0500

    add new package extra_functions.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from maths.basic_functions import add
+from maths.extra_functions import tables
 
 # call functions
 
@@ -9,3 +10,9 @@ numberTwo = int(input("enter second number: "))
 result = add(numberOne, numberTwo)
 print()
 print("the result is: {}".format(result))
+
+
+print("call function table:")
+
+tables(5)
+
